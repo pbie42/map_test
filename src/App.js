e82ec26e9bee083657b583667b9413bf70f359be
Paul Bie <pbie@student.42.fr>
2018-12-11 10:05:55 +0100

    [Add] - Deck gl mapbox map

--- src/App.js
+++ src/App.js
@@ -1,28 +1,186 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+import { StaticMap } from 'react-map-gl'
+import DeckGL, { LineLayer, ScatterplotLayer } from 'deck.gl'
+import GL from 'luma.gl/constants'
+import test from './test'
+import test2 from './test2'
+
+// Set your mapbox token here
+const MAPBOX_TOKEN =
+  'pk.eyJ1IjoidmluY2VjaXR5MzAwMCIsImEiOiI1QVZUTlNVIn0.r1aM4ZjV__LejQkARJ5wGQ' // eslint-disable-line
+
+// Source data CSV
+const DATA_URL = {
+  AIRPORTS:
+    'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/line/airports.json', // eslint-disable-line
+  FLIGHT_PATHS:
+    'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/line/heathrow-flights.json' // eslint-disable-line
+}
+
+export const INITIAL_VIEW_STATE = {
+  latitude: 45.815399,
+  longitude: 15.966568,
+  zoom: 4.5,
+  maxZoom: 16,
+  pitch: 50,
+  bearing: 0
+}
+
+function getColor(d) {
+  const z = d.start[2] < d.end[2] ? d.start[2] : d.end[2]
+  console.log(`z`, z)
+  const r = z / 10000
+
+  return shadeRGBColor(
+    [255 * (1 - r * 2), 128 * r, 255 * r, 255 * (1 - r)],
+    0.25
+  )
+}
+
+function shadeRGBColor(color, percent) {
+  var f = color,
+    t = percent < 0 ? 0 : 255,
+    p = percent < 0 ? percent * -1 : percent,
+    R = parseInt(f[0]),
+    G = parseInt(f[1]),
+    B = parseInt(f[2])
+  return [
+    Math.round((t - R) * p) + R,
+    Math.round((t - G) * p) + G,
+    Math.round((t - B) * p) + B
+  ]
+}
+
+function getSize(type) {
+  if (type.search('major') >= 0) {
+    return 100
+  }
+  if (type.search('small') >= 0) {
+    return 30
+  }
+  return 60
+}
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hoveredObject: null
+    }
+    this._onHover = this._onHover.bind(this)
+    this._renderTooltip = this._renderTooltip.bind(this)
+  }
+
+  _onHover({ x, y, object }) {
+    this.setState({ x, y, hoveredObject: object })
+  }
+
+  _renderTooltip() {
+    const { x, y, hoveredObject } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      hoveredObject && (
+        <div className="tooltip" style={{ left: x, top: y }}>
+          <div>{hoveredObject.country || hoveredObject.abbrev}</div>
+          <div>
+            {hoveredObject.name.indexOf('0x') >= 0 ? '' : hoveredObject.name}
+          </div>
+        </div>
+      )
+    )
+  }
+
+  _renderLayers() {
+    const {
+      airports = DATA_URL.AIRPORTS,
+      flightPaths = DATA_URL.FLIGHT_PATHS,
+      getStrokeWidth = 3
+    } = this.props
+
+    const newTest = []
+    for (let i = 0; i < test2.length; i++) {
+      if (test2[i] && test2[i + 1])
+        newTest.push({
+          start: [test2[i].lon, test2[i].lat, test2[i].alt],
+          end: [test2[i + 1].lon, test2[i + 1].lat, test2[i + 1].alt],
+          name: 'sumish'
+        })
+    }
+    console.log(`flightPaths`, flightPaths)
+    // for (let i = 0; i < test.length; i += 1) {
+    //   if (test[i] && test[i + 1])
+    //     newTest.push({
+    //       start: [test[i][0], test[i][1], i * 100],
+    //       end: [test[i + 1][0], test[i + 1][1], randomIntFromInterval(1, 70)],
+    //       name: 'SASHIT'
+    //     })
+    // }
+
+    console.log(`newTest`, newTest)
+
+    return [
+      new ScatterplotLayer({
+        id: 'airports',
+        data: airports,
+        radiusScale: 20,
+        getPosition: d => d.coordinates,
+        getColor: [255, 140, 0],
+        getRadius: d => getSize(d.type),
+        pickable: true,
+        onHover: this._onHover
+      }),
+      new LineLayer({
+        id: 'flight-paths',
+        data: newTest,
+        fp64: false,
+        getSourcePosition: d => d.start,
+        getTargetPosition: d => d.end,
+        getColor,
+        getStrokeWidth,
+        pickable: true,
+        onHover: this._onHover
+      })
+    ]
+  }
+
+  render() {
+    const { viewState, controller = true, baseMap = true } = this.props
+
+    return (
+      <DeckGL
+        layers={this._renderLayers()}
+        initialViewState={INITIAL_VIEW_STATE}
+        viewState={viewState}
+        controller={controller}
+        pickingRadius={5}
+        parameters={{
+          blendFunc: [GL.SRC_ALPHA, GL.ONE, GL.ONE_MINUS_DST_ALPHA, GL.ONE],
+          blendEquation: GL.FUNC_ADD
+        }}
+      >
+        {baseMap && (
+          <StaticMap
+            reuseMaps
+            mapStyle="mapbox://styles/vincecity3000/cjoygjxhs3xc92smrtujp8rem"
+            preventStyleDiffing={true}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+          />
+        )}
+
+        {this._renderTooltip}
+      </DeckGL>
+    )
   }
 }
 
-export default App;
+export default App
+
+// export function renderToDOM(container) {
+//   render(<App />, container);
+// }
---
+++ src/test2.js
@@ -0,0 +1,197 @@
+export default [
+  {
+    type: 'APT',
+    ident: 'LFPG',
+    name: 'Paris Charles De Gaulle',
+    alt: 0,
+    lat: 49.00970000000001,
+    lon: 2.5626200000000097,
+    via: null
+  },
+  {
+    type: 'FIX',
+    ident: 'LASIV',
+    name: null,
+    alt: 23500,
+    lat: 48.900800000000004,
+    lon: 4.226940000000013,
+    via: null
+  },
+  {
+    type: 'FIX',
+    ident: 'LANVI',
+    name: null,
+    alt: 35000,
+    lat: 48.3117,
+    lon: 5.79722000000001,
+    via: {
+      type: 'AWY-HI',
+      ident: 'UL161'
+    }
+  },
+  {
+    type: 'VOR',
+    ident: 'EPL',
+    name: 'EPINAL MIRECOURT',
+    alt: 35000,
+    lat: 48.317800000000005,
+    lon: 6.059419999999989,
+    via: {
+      type: 'AWY-HI',
+      ident: 'UM164'
+    }
+  },
+  {
+    type: 'FIX',
+    ident: 'LASAT',
+    name: null,
+    alt: 35000,
+    lat: 48.15890000000002,
+    lon: 6.684169999999995,
+    via: {
+      type: 'AWY-HI',
+      ident: 'UN491'
+    }
+  },
+  {
+    type: 'FIX',
+    ident: 'BEGAR',
+    name: null,
+    alt: 35000,
+    lat: 47.9083,
+    lon: 7.5833299999999895,
+    via: {
+      type: 'AWY-HI',
+      ident: 'UN491'
+    }
+  },
+  {
+    type: 'FIX',
+    ident: 'LENGI',
+    name: null,
+    alt: 35000,
+    lat: 47.7792,
+    lon: 8.076940000000008,
+    via: {
+      type: 'AWY-HI',
+      ident: 'UN491'
+    }
+  },
+  {
+    type: 'VOR',
+    ident: 'TRA',
+    name: 'TRASADINGEN',
+    alt: 35000,
+    lat: 47.68950000000001,
+    lon: 8.436949999999996,
+    via: {
+      type: 'AWY-HI',
+      ident: 'UN491'
+    }
+  },
+  {
+    type: 'VOR',
+    ident: 'ZUE',
+    name: 'ZURICH',
+    alt: 35000,
+    lat: 47.59219999999999,
+    lon: 8.817669999999993,
+    via: {
+      type: 'AWY-HI',
+      ident: 'UN491'
+    }
+  },
+  {
+    type: 'FIX',
+    ident: 'DEGES',
+    name: null,
+    alt: 35000,
+    lat: 47.412499999999994,
+    lon: 9.201940000000008,
+    via: {
+      type: 'AWY-HI',
+      ident: 'UN491'
+    }
+  },
+  {
+    type: 'FIX',
+    ident: 'GAMSA',
+    name: null,
+    alt: 35000,
+    lat: 47.408500000000004,
+    lon: 9.651909999999987,
+    via: {
+      type: 'AWY-HI',
+      ident: 'UN871'
+    }
+  },
+  {
+    type: 'FIX',
+    ident: 'MADEB',
+    name: null,
+    alt: 35000,
+    lat: 47.3244,
+    lon: 10.288900000000012,
+    via: {
+      type: 'AWY-HI',
+      ident: 'UN871'
+    }
+  },
+  {
+    type: 'FIX',
+    ident: 'ELMEM',
+    name: null,
+    alt: 35000,
+    lat: 47.28559999999999,
+    lon: 10.570699999999988,
+    via: {
+      type: 'AWY-LO',
+      ident: 'N606'
+    }
+  },
+  {
+    type: 'FIX',
+    ident: 'LIZUM',
+    name: null,
+    alt: 35000,
+    lat: 47.115099999999984,
+    lon: 11.756,
+    via: {
+      type: 'AWY-HI',
+      ident: 'UL607'
+    }
+  },
+  {
+    type: 'FIX',
+    ident: 'VEKEN',
+    name: null,
+    alt: 26100,
+    lat: 46.56360000000001,
+    lon: 13.379400000000004,
+    via: {
+      type: 'AWY-HI',
+      ident: 'UL607'
+    }
+  },
+  {
+    type: 'FIX',
+    ident: 'DEGUM',
+    name: null,
+    alt: 23000,
+    lat: 46.465900000000005,
+    lon: 13.699299999999994,
+    via: {
+      type: 'AWY-HI',
+      ident: 'UY177'
+    }
+  },
+  {
+    type: 'APT',
+    ident: 'LDZA',
+    name: 'Zagreb',
+    alt: 0,
+    lat: 45.74180000000001,
+    lon: 16.067200000000014,
+    via: null
+  }
+]
